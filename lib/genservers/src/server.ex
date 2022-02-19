54a83894885a69c05a5b3cb2090023c26230359a
vKxni <Konradtoesch@icloud.com>
2022-02-19 17:08:29 +0100

    docs

--- lib/genservers/src/server.ex
+++ lib/genservers/src/server.ex
@@ -8,12 +8,13 @@ defmodule Stack do
     GenServer.start_link(__MODULE__, default)
   end
 
-  # cast does not receive messages
+  # cast does not receive or care about any messages
   def push(pid, element) do
   GenServer.cast(pid, {:push, element})
   end
 
-  # defining :pop for the GenServer_call
+  # defining :pop for the GenServer_call / handle call below
+  # here we care about messages and we receive them from the server.
   def pop(pid) do
   GenServer.call(pid, :pop)
   end
@@ -24,13 +25,13 @@ defmodule Stack do
     {:ok, stack}
   end
 
-  # does receive messages
+  # does receive messages / we care about them
   @impl true
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
 
-  # does not receive messages
+  # does not receive messages / we dont care about them
   @impl true
   def handle_cast({:push, element}, state) do
     {:noreply, [element | state]}
